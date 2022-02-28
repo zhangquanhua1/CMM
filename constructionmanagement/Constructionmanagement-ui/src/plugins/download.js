f33e5ff6596a1fe402efc6960266be4ccabe1086
oneday <476303084@qq.com>
2022-02-28 10:05:32 +0800

    修改配置

--- constructionmanagement/Constructionmanagement-ui/src/plugins/download.js
+++ constructionmanagement/Constructionmanagement-ui/src/plugins/download.js
@@ -1,5 +1,7 @@
+//0
 import axios from 'axios'
 import { Message } from 'element-ui'
+//导出文件
 import { saveAs } from 'file-saver'
 import { getToken } from '@/utils/auth'
 import { blobValidate } from "@/utils/ruoyi";
@@ -7,6 +9,7 @@ import { blobValidate } from "@/utils/ruoyi";
 const baseURL = process.env.VUE_APP_BASE_API
 
 export default {
+
   name(name, isDelete = true) {
     var url = baseURL + "/common/download?fileName=" + encodeURI(name) + "&delete=" + isDelete
     axios({
@@ -24,6 +27,7 @@ export default {
       }
     })
   },
+
   resource(resource) {
     var url = baseURL + "/common/download/resource?resource=" + encodeURI(resource);
     axios({
@@ -41,23 +45,24 @@ export default {
       }
     })
   },
-  zip(url, name) {
-    var url = baseURL + url
-    axios({
-      method: 'get',
-      url: url,
-      responseType: 'blob',
-      headers: { 'Authorization': 'Bearer ' + getToken() }
-    }).then(async (res) => {
-      const isLogin = await blobValidate(res.data);
-      if (isLogin) {
-        const blob = new Blob([res.data], { type: 'application/zip' })
-        this.saveAs(blob, name)
-      } else {
-        Message.error('无效的会话，或者会话已过期，请重新登录。');
-      }
-    })
-  },
+
+  // zip(url, name) {
+  //   var url = baseURL + url
+  //   axios({
+  //     method: 'get',
+  //     url: url,
+  //     responseType: 'blob',
+  //     headers: { 'Authorization': 'Bearer ' + getToken() }
+  //   }).then(async (res) => {
+  //     const isLogin = await blobValidate(res.data);
+  //     if (isLogin) {
+  //       const blob = new Blob([res.data], { type: 'application/zip' })
+  //       this.saveAs(blob, name)
+  //     } else {
+  //       Message.error('无效的会话，或者会话已过期，请重新登录。');
+  //     }
+  //   })
+  // },
   saveAs(text, name, opts) {
     saveAs(text, name, opts);
   }
